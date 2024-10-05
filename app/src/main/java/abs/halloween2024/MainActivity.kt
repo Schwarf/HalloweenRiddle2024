f24bca56639d014fa03cddee77bd7180c8dd6733
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-05 11:56:35 +0200

    Add glowing to text.

--- app/src/main/java/abs/halloween2024/MainActivity.kt
+++ app/src/main/java/abs/halloween2024/MainActivity.kt
@@ -12,12 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import abs.halloween2024.ui.theme.HalloweenRiddle2024Theme
+import android.graphics.PointF
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Button
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -86,16 +95,10 @@ fun Luigina(
             modifier = Modifier.matchParentSize()
         )
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "Das ist meine Schwester Luigina. Die gemeinen Geister haben sie gejagt." +
+                GlowingText(text = "Das ist meine Schwester Luigina. Die gemeinen Geister haben sie gejagt." +
                         "Da es so viele Geister waren, hat sie sich wohl versteckt. " +
-                        "Vielleicht finden wir eine Nachricht von ihr.",
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    fontSize = 25.sp
-                ),
-            )
+                        "Vielleicht finden wir eine Nachricht von ihr.", glowColor = Color.Green, textColor = Color.White)
+
 //            Button(onClick = {onButtonClick()},
 //            ) {
 //                Text(text = "Ja!")
@@ -106,6 +109,30 @@ fun Luigina(
 
 }
 
+@Composable
+fun GlowingText(text: String, glowColor: Color, textColor: Color) {
+    BasicText(
+        text = text,
+        style = TextStyle(
+            color = textColor,
+            fontSize = 24.sp,
+            shadow = Shadow(
+                color = glowColor,
+                offset = Offset(0f, 0f),
+                blurRadius = 16f
+            )
+        ),
+        modifier = Modifier.graphicsLayer(alpha = 0.99f).drawWithContent {
+            drawContent()
+            withTransform({
+                translate(left = 4f, top = 4f)
+                scale(scaleX = 1.01f, scaleY = 1.01f)
+            }) {
+                this@drawWithContent.drawContent()
+            }
+        }
+    )
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
